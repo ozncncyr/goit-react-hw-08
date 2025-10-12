ea846dceb317fadc68adf4cf1e830a7d3f8073b5
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-10-12 20:18:45 +0300

    fix: improve loading state management in auth slice for register and logIn actions

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -9,6 +9,15 @@ const initialState = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  isLoading: false,
+};
+
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state) => {
+  state.isLoading = false;
 };
 
 const authSlice = createSlice({
@@ -16,16 +25,22 @@ const authSlice = createSlice({
   initialState,
   extraReducers: (builder) => {
     builder
+      .addCase(register.pending, handlePending)
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.isLoading = false;
       })
+      .addCase(register.rejected, handleRejected)
+      .addCase(logIn.pending, handlePending)
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.isLoading = false;
       })
+      .addCase(logIn.rejected, handleRejected)
       .addCase(logOut.fulfilled, (state) => {
         state.user = { name: null, email: null };
         state.token = null;
